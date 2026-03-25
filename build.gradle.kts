28f0e5d9d4ea47a51d12d5eb87706071173388e6
Prudhvi Arthimalla <prudhvi@Prudhvis-MacBook-Pro.local>
2026-03-25 19:25:29 -0400

    Remove Foojay plugin, switch toolchain to Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ subprojects {
 
     configure<JavaPluginExtension> {
         toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
+            languageVersion = JavaLanguageVersion.of(25)
         }
     }
 }
