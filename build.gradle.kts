7e012ee1053528472fba01e3d180e7e4911cb1c1
Prudhvi Arthimalla <prudhvi@Prudhvis-MacBook-Pro.local>
2026-03-25 19:18:09 -0400

    Add root Gradle multi-project build files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java apply false
+    id("org.springframework.boot") version "3.4.3" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    repositories {
+        mavenCentral()
+    }
+
+    group = "com.rideshare"
+    version = "0.0.1-SNAPSHOT"
+
+    configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+}
